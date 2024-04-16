0dd5861383de2d8d9c12e40d366564fdde6a62d7
drame_c <drame_c@etna-alternance.net>
2024-04-16 19:40:44 +0200

    subject : add, delete, edit, show

--- src/Controller/SubjectController.php
+++ src/Controller/SubjectController.php
@@ -2,42 +2,106 @@
 
 namespace App\Controller;
 
-use App\Entity\Basket;
 use App\Entity\Subject;
+use App\Form\SubjectFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SubjectController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
 
-    // #[Route('/subject', name: 'app_subject')]
-    // public function subject(EntityManagerInterface $entityManager): Response
-    // {
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
-    //     $user = $this->getUser();
-    //     if (!$user) {
-    //         $this->addFlash('error', 'Vous devez être connecté pour accéder au forum.');
-    //         return $this->redirectToRoute('app_login'); 
-    //     }
+    #[Route('/subject', name: 'app_subject')]
+    public function subject(Request $request): Response
+    {
+        $subjects = $this->entityManager->getRepository(Subject::class)->findAll();
+        $subject = $this->entityManager->getRepository(Subject::class)->findAll();
 
-    //     $subjectRepository = $entityManager->getRepository(Subject::class);
-    //     $existingSubjects = $subjectRepository->findAll();
+        return $this->render('subject/index.html.twig', [
+            'subjects' => $subjects,
+            'subject' => $subject,
+        ]);
+    }
 
-    //     if (!empty($existingSubjects)) {
-    //         return $this->render('subject/index.html.twig', [
-    //             'subjects' => $existingSubjects,
-    //         ]);
-    //     }
+    #[Route('/subject/create', name: 'app_subject_create')]
+    public function addSubject(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
 
-    //     $subject = new Subject();
-    //     $subject->setName('Movies');
-    //     $subject->setDescription('Œuvre cinématographique enregistrée sur film (cinéma).');
+        $subject = new Subject();
+        $form = $this->createForm(SubjectFormType::class, $subject);
+        $form->handleRequest($request);
 
-    //     $entityManager->persist($subject);
-    //     $entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $subject->setCategory($form->get('theme')->getData());
+            $subject->setOwner($this->getUser());
 
-    //     return $this->redirectToRoute('app_subject');
-    // }
+            $this->entityManager->persist($subject);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Nouveau sujet créé !');
+
+            return $this->redirectToRoute('app_subject_create');
+        }
+
+        return $this->render('subject/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/subject/edit', name: 'app_subject_edit')]
+    public function editSubject(Request $request, Subject $subject): Response
+    {
+        $form = $this->createForm(SubjectFormType::class, $subject);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($subject);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Le sujet a été modifié avec succès.');
+
+            return $this->redirectToRoute('app_subject_edit');
+        }
+
+        return $this->render('subject/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/subject/delete', name: 'app_subject_delete')]
+    public function deleteSubject(Request $request): Response
+    {
+        $subjects = $this->entityManager->getRepository(Subject::class)->findAll();
+
+        if ($request->isMethod('POST')) {
+            $submittedData = $request->request->all();
+            $subjectId = $submittedData['subject_id'];
+
+            $subject = $this->entityManager->getRepository(Subject::class)->find($subjectId);
+
+            if ($subject) {
+                $this->entityManager->remove($subject);
+                $this->entityManager->flush();
+
+                $this->addFlash('success', 'Le sujet a été supprimé avec succès.');
+            } else {
+                $this->addFlash('error', 'Le sujet n\'a pas été trouvé.');
+            }
+
+            return $this->redirectToRoute('app_subject_delete');
+        }
+
+        return $this->render('subject/delete.html.twig', [
+            'subjects' => $subjects,
+        ]);
+    }
 }
---
+++ src/Form/SubjectFormType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Subject;
+use App\Entity\Theme;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SubjectFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+            ])
+            ->add('theme', EntityType::class, [
+                'class' => Theme::class,
+                'choice_label' => 'name',
+                'label' => 'Thème',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Subject::class,
+        ]);
+    }
+}
+
+
+        // $builder
+        //     ->add('name')
+        //     ->add('description')
+        //     ->add('category', EntityType::class, [
+        //         'class' => Theme::class,
+        //         'choice_label' => 'id',
+        //     ])
+        //     ->add('owner', EntityType::class, [
+        //         'class' => User::class,
+        //         'choice_label' => 'id',
+        //     ])
+        //     ->add('theme', EntityType::class, [
+        //         'class' => Theme::class,
+        //         'choice_label' => 'id',
+        //     ])
+        // ;
