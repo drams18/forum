9cd56cf5c2e2dd27ac8db8ec1bafad8f635b458e
drame_c <drame_c@etna-alternance.net>
2024-04-22 17:51:32 +0200

    update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
     #[Route('/home', name: 'app_home')]
     public function home(): Response
     {
+        if (!$this->security->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
