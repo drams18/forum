0a87d4d65886972436d120b13375de9b62be275b
drame_c <drame_c@etna-alternance.net>
2024-04-22 17:01:26 +0200

    first

---
+++ src/Controller/PostController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostFormType;
+use App\Repository\PostRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+
+{
+    private EntityManagerInterface $entityManager;
+    private Security $security;
+    private PostRepository $postRepository;
+
+    public function __construct(EntityManagerInterface $entityManager, Security $security, PostRepository $postRepository)
+    {
+        $this->entityManager = $entityManager;
+        $this->security = $security;
+        $this->postRepository = $postRepository;
+    }
+
+    #[Route('/posts', name: 'app_post')]
+    public function showAllPosts(): Response
+    {
+        $posts = $this->entityManager->getRepository(Post::class)->findAll();
+
+        return $this->render('post/show.html.twig',  [
+            'posts' => $posts,
+        ]);
+    }
+
+    #[Route('/post-add', name: 'app_post_create')]
+    public function addPost(Request $request): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostFormType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setSubject($form->get('subject')->getData());
+
+            $user = $this->security->getUser();
+            $post->setOwner($user);
+            $post->setCreatedAt(new DateTimeImmutable());
+            $post->setUpdatedAt(new DateTimeImmutable());
+
+            $this->entityManager->persist($post);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Post créé avec succès.');
+
+            return $this->redirectToRoute('app_post');
+        }
+
+        return $this->render('post/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/post/{id}', name: 'app_post_details')]
+    public function showPostDetails(Post $post): Response
+    {
+        return $this->render('post/post.details.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/post-delete/{id}', name: 'app_post_delete')]
+    public function deletePost(Post $post): RedirectResponse
+    {
+        $this->entityManager->remove($post);
+        $this->entityManager->flush();
+
+        $this->addFlash('success', 'Le post a été supprimé avec succès.');
+
+        return $this->redirectToRoute('app_post');
+    }
+
+    #[Route('/post-edit/{id}', name: 'app_post_edit')]
+    public function editPost(Request $request, Post $post): Response
+    {
+        $form = $this->createForm(PostFormType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setUpdatedAt(new DateTimeImmutable());
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Le post a été modifié avec succès.');
+
+            return $this->redirectToRoute('app_post');
+        }
+
+        return $this->render('post/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/PostFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use App\Entity\Subject;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('content')
+            ->add('subject', EntityType::class, [
+                'class' => Subject::class,
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
